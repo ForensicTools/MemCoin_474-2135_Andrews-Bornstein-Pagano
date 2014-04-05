feace4aec38192350158f5375eabc792b965e96d
sxp1998 <sxp1998@rit.edu>
2014-04-05 19:21:40 -0400

    Update MEMcoin.pl
    Added a search function for Wallets and cleaned up all of the code (more functions were added).

--- MEMcoin.pl
+++ MEMcoin.pl
@@ -1,51 +1,82 @@
 use strict;
 use warnings;
-system("pwd");
-#if sudo install fmem
-chdir("fmem_1.6-0/");
-system("pwd");
-system("make");
 
-# Revision - if fmem install fails due to kernal headers report failure
-system("./run.sh");
-chdir("../");
-system("pwd");
-chdir("MemoryImages");
+fmemSetup();
+searchForWallets();
+imageMemory();
+memorySearch();
 
-#grab an image
-system("dd if=/dev/fmem of=investigatedcompmem");
-system("cat investigatedcompmem | strings > memstrings");
+sub fmemSetup{
 
-#check to see if password exists
-my @strings=();
-my $flag = 0;
+	system("pwd");
+	#if sudo install fmem
+	chdir("fmem_1.6-0/");
+	system("pwd");
+	system("make");
 
-my $file = 'memstrings';
-open my $fh, '<', $file or die "Could not open '$file' $!\n";
- 
- 
-#get 3 lines after regex in memory dump 
-while (my $line = <$fh>) 
-{
-   
-   chomp $line;
+	# Revision - if fmem install fails due to kernal headers report 	failure
+	system("./run.sh");
+	chdir("../");
+	system("mkdir InvestigationInfo");
+	chdir("InvestigationInfo");
+	system("mkdir InvestigationInfo");
+	system("pwd");
 
-   if($flag > 0)
-   {
 
-	print "$line\n";
-	print "$flag\n";	
-	$flag--;
+}
+sub searchForWallets{
+
+	print "Searching for Wallets...\n";
+	system("find / -name \*.wallet > WalletInfoFound");
+
+}
+
+sub imageMemory{
+
+	#grab an image
+	print "Grabbing an Image of Memory...\n";
+	system("dd if=/dev/fmem of=investigatedcompmem");
+	print "MEMCoin is making sense of all this data...\n";
+	system("cat investigatedcompmem | strings > memstrings");
+
+}
+
+sub memorySearch{
+
+	#check to see if password exists
+	print "Checking to see if passwords exist...\n";
+	my @strings=();
+	my $flag = 0;
+
+	my $file = 'memstrings';
+	open my $fh, '<', $file or die "Could not open '$file' $!\n";
+
+	system("touch PossiblePasswords");
+	open(FILE, ">>PossiblePasswords") || die("Error could not open file");
+	 
+	while (my $line = <$fh>) 
+	{
+	   
+	   chomp $line;
+
+	   if($flag > 0)
+	   {
+
+		print FILE "$line\n";	
+		$flag--;
 	
 
-   }
+	   }
 
-  # need to figure out regex
-   if($line =~ '')
-   {
-	print "$line \n";
-	$flag =3;
+	  #regex not listed
+	   if($line =~ '')
+	   {
+		print FILE "$line \n";
+		$flag =3;
 
-   }
+	   }
 
+	}
+
+	print "MEMCoin has finished investigation...\n";
 }
